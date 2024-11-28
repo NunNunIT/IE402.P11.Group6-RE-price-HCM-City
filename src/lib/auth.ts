0721fbdad3f7f4f2b29b5964294a1a6a276a61e6
fredle09 <le197903@gmail.com>
2024-11-28 21:36:19 +0700

    feat: integrate next-auth for authentication with GitHub provider

---
+++ src/lib/auth.ts
@@ -0,0 +1,9 @@
+import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
+  callbacks: {
+    authorized: async ({ auth }) => !!auth,
+  },
+})
--- src/provider/global.tsx
+++ src/provider/global.tsx
@@ -1,16 +1,21 @@
+import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from ".";
 import { Toaster } from "@/components/ui/sonner";
+import { auth } from "@/lib/auth";
 
+export default async function GlobalProvider({ children }: { children: React.ReactNode }) {
+  const session = await auth();
 
-export default function GlobalProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-    >
-      {children}
-      <Toaster />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+      >
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
