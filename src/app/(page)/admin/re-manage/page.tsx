44a0ef699149c05cb20da8bcaa943fb6323d6204
NunNunIT <hongnhung02v04@gmail.com>
2024-12-15 15:05:41 +0700

    merger

--- src/app/(page)/admin/re-manage/page.tsx
+++ src/app/(page)/admin/re-manage/page.tsx
@@ -1,7 +1,58 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-[100dvh]">
+import { DataColumns, columns } from "./columns";
+import { DataTable } from "./data-table";
 
+async function getData(): Promise<DataColumns[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      _id: "728ed52f",
+      title: "Tên bất động sản",
+      price: 100,
+      area: 25,
+      fullAddress: "Địa chỉ đầy đủ đã qua chỉnh sửa ở API 6",
+      isAuth: false,
+    },
+    {
+      _id: "728ed52f",
+      title: "Tên bất động sản",
+      price: 100,
+      area: 25,
+      fullAddress: "Địa chỉ đầy đủ đã qua chỉnh sửa ở API 5",
+      isAuth: false,
+    },
+    {
+      _id: "728ed52f",
+      title: "Tên bất động sản",
+      price: 100,
+      area: 25,
+      fullAddress: "Địa chỉ đầy đủ đã qua chỉnh sửa ở API 4",
+      isAuth: false,
+    },
+    {
+      _id: "728ed52f",
+      title: "Tên bất động sản",
+      price: 100,
+      area: 25,
+      fullAddress: "Địa chỉ đầy đủ đã qua chỉnh sửa ở API 3",
+      isAuth: false,
+    },
+    {
+      _id: "728ed52f",
+      title: "Tên bất động sản",
+      price: 100,
+      area: 25,
+      fullAddress: "Địa chỉ đầy đủ đã qua chỉnh sửa ở API 2",
+      isAuth: false,
+    },
+  ];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10 px-2">
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
