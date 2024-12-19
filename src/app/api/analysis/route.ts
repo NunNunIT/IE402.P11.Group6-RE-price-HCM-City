f62c154fd82410f4d998625ac78d407ab3622d81
fredle09 <le197903@gmail.com>
2024-12-19 22:31:19 +0700

    feat: enhance breadcrumb component with dynamic title fetching and improved routing

--- src/app/api/analysis/route.ts
+++ src/app/api/analysis/route.ts
@@ -8,199 +8,201 @@ export const GET = async (req: NextRequest) => {
   const district = searchParams.get("district");
   const ward = searchParams.get("ward");
 
-  try {
-    const currentYear = new Date().getFullYear().toString();
+  return successResponse({});
 
-    const currentMonth = (new Date().getMonth() + 1)
-      .toString()
-      .padStart(2, "0");
+  // try {
+  //   const currentYear = new Date().getFullYear().toString();
 
-    // // 1. Lấy danh sách District khi chỉ có Province
-    // if (province && !district) {
-    //   const provinceData = await Province.findOne({
-    //     name: { $regex: province, $options: "i" },
-    //   });
+  //   const currentMonth = (new Date().getMonth() + 1)
+  //     .toString()
+  //     .padStart(2, "0");
 
-    //   if (!provinceData) {
-    //     return errorResponse({ message: "Province not found" });
-    //   }
+  //   // // 1. Lấy danh sách District khi chỉ có Province
+  //   // if (province && !district) {
+  //   //   const provinceData = await Province.findOne({
+  //   //     name: { $regex: province, $options: "i" },
+  //   //   });
 
-    //   const districts = await District.find({
-    //     province: provinceData._id,
-    //   }).lean();
+  //   //   if (!provinceData) {
+  //   //     return errorResponse({ message: "Province not found" });
+  //   //   }
 
-    //   const districtWithAnalysis = districts.map((d) => ({
-    //     name: d.name,
-    //     analysis: d.analysis[currentYear]?.[currentMonth] || null,
-    //   }));
+  //   //   const districts = await District.find({
+  //   //     province: provinceData._id,
+  //   //   }).lean();
 
-    //   return successResponse({
-    //     data: {
-    //       province: provinceData.name,
-    //       analysis: provinceData.analysis[currentYear]?.[currentMonth] || null,
-    //       districts: districts.map((d) => d.name),
-    //     },
-    //     message: "Danh sách quận/huyện và dữ liệu phân tích",
-    //   });
-    // }
+  //   //   const districtWithAnalysis = districts.map((d) => ({
+  //   //     name: d.name,
+  //   //     analysis: d.analysis[currentYear]?.[currentMonth] || null,
+  //   //   }));
 
-    // 1. Trường hợp chỉ chọn Province
-    if (province && !district) {
-      const provinceData = await Province.findOne({
-        name: { $regex: province, $options: "i" },
-      });
+  //   //   return successResponse({
+  //   //     data: {
+  //   //       province: provinceData.name,
+  //   //       analysis: provinceData.analysis[currentYear]?.[currentMonth] || null,
+  //   //       districts: districts.map((d) => d.name),
+  //   //     },
+  //   //     message: "Danh sách quận/huyện và dữ liệu phân tích",
+  //   //   });
+  //   // }
 
-      if (!provinceData)
-        return errorResponse({ message: "Province not found" });
+  //   // 1. Trường hợp chỉ chọn Province
+  //   if (province && !district) {
+  //     const provinceData = await Province.findOne({
+  //       name: { $regex: province, $options: "i" },
+  //     });
 
-      const districts = await District.find({
-        province: provinceData._id,
-      }).lean();
+  //     if (!provinceData)
+  //       return errorResponse({ message: "Province not found" });
 
-      const districtWithAnalysis = districts.map((d: any) => ({
-        name: d.name,
-        analysis: d.analysis[currentYear]?.[currentMonth] || null,
-      }));
+  //     const districts = await District.find({
+  //       province: provinceData._id,
+  //     }).lean();
 
-      return successResponse({
-        data: {
-          province: provinceData.name,
-          analysis: provinceData.analysis[currentYear]?.[currentMonth] || null,
-          districts: districtWithAnalysis,
-        },
-        message: "Danh sách quận/huyện và giá trị phân tích",
-      });
-    }
+  //     const districtWithAnalysis = districts.map((d: any) => ({
+  //       name: d.name,
+  //       analysis: d.analysis[currentYear]?.[currentMonth] || null,
+  //     }));
 
-    //     // 2. Lấy danh sách Ward khi có Province và District
-    //     if (province && district && !ward) {
-    //       const provinceData = await Province.findOne({
-    //         name: { $regex: province, $options: "i" },
-    //       });
-    //       const districtData = await District.findOne({
-    //         name: { $regex: district, $options: "i" },
-    //         province: provinceData._id,
-    //       });
+  //     return successResponse({
+  //       data: {
+  //         province: provinceData.name,
+  //         analysis: provinceData.analysis[currentYear]?.[currentMonth] || null,
+  //         districts: districtWithAnalysis,
+  //       },
+  //       message: "Danh sách quận/huyện và giá trị phân tích",
+  //     });
+  //   }
 
-    //       if (!districtData) {
-    //         return errorResponse({ message: "District not found" });
-    //       }
+  //   //     // 2. Lấy danh sách Ward khi có Province và District
+  //   //     if (province && district && !ward) {
+  //   //       const provinceData = await Province.findOne({
+  //   //         name: { $regex: province, $options: "i" },
+  //   //       });
+  //   //       const districtData = await District.findOne({
+  //   //         name: { $regex: district, $options: "i" },
+  //   //         province: provinceData._id,
+  //   //       });
 
-    //       const wards = await Ward.find({ district: districtData._id }).lean();
+  //   //       if (!districtData) {
+  //   //         return errorResponse({ message: "District not found" });
+  //   //       }
 
-    //       return successResponse({
-    //         data: {
-    //           province: provinceData.name,
-    //           district: districtData.name,
-    //           analysis: districtData.analysis[currentYear]?.[currentMonth] || null,
-    //           wards: wards.map((w) => w.name),
-    //         },
-    //         message: "Danh sách phường/xã và dữ liệu phân tích",
-    //       });
-    //     }
+  //   //       const wards = await Ward.find({ district: districtData._id }).lean();
 
-    //     // 3. Lấy thông tin Ward khi có đủ Province, District và Ward
-    //     if (province && district && ward) {
-    //       const provinceData = await Province.findOne({
-    //         name: { $regex: province, $options: "i" },
-    //       });
-    //       const districtData = await District.findOne({
-    //         name: { $regex: district, $options: "i" },
-    //         province: provinceData._id,
-    //       });
-    //       const wardData = await Ward.findOne({
-    //         name: { $regex: ward, $options: "i" },
-    //         district: districtData._id,
-    //       });
+  //   //       return successResponse({
+  //   //         data: {
+  //   //           province: provinceData.name,
+  //   //           district: districtData.name,
+  //   //           analysis: districtData.analysis[currentYear]?.[currentMonth] || null,
+  //   //           wards: wards.map((w) => w.name),
+  //   //         },
+  //   //         message: "Danh sách phường/xã và dữ liệu phân tích",
+  //   //       });
+  //   //     }
 
-    //       const wards = await Ward.find({ district: districtData._id }).lean();
+  //   //     // 3. Lấy thông tin Ward khi có đủ Province, District và Ward
+  //   //     if (province && district && ward) {
+  //   //       const provinceData = await Province.findOne({
+  //   //         name: { $regex: province, $options: "i" },
+  //   //       });
+  //   //       const districtData = await District.findOne({
+  //   //         name: { $regex: district, $options: "i" },
+  //   //         province: provinceData._id,
+  //   //       });
+  //   //       const wardData = await Ward.findOne({
+  //   //         name: { $regex: ward, $options: "i" },
+  //   //         district: districtData._id,
+  //   //       });
 
-    //       if (!wardData) {
-    //         return errorResponse({ message: "Ward not found" });
-    //       }
+  //   //       const wards = await Ward.find({ district: districtData._id }).lean();
 
-    //       return successResponse({
-    //         data: {
-    //           district: districtData.name,
-    //           analysis: wardData.analysis[currentYear]?.[currentMonth] || null,
-    //           wards: wards.map((w) => w.name),
-    //         },
-    //         message: "Dữ liệu phân tích của phường/xã",
-    //       });
-    //     }
+  //   //       if (!wardData) {
+  //   //         return errorResponse({ message: "Ward not found" });
+  //   //       }
 
-    //     return errorResponse({ message: "Invalid parameters" });
-    //   } catch (error) {
-    //     console.error("Error fetching data:", error);
-    //     return errorResponse({ message: "Đã có lỗi xảy ra", error: error.message });
-    //   }
-    // };
+  //   //       return successResponse({
+  //   //         data: {
+  //   //           district: districtData.name,
+  //   //           analysis: wardData.analysis[currentYear]?.[currentMonth] || null,
+  //   //           wards: wards.map((w) => w.name),
+  //   //         },
+  //   //         message: "Dữ liệu phân tích của phường/xã",
+  //   //       });
+  //   //     }
 
-    // 2. Trường hợp chọn Province và District
-    if (province && district && !ward) {
-      const provinceData = await Province.findOne({
-        name: { $regex: province, $options: "i" },
-      });
-      const districtData = await District.findOne({
-        name: { $regex: district, $options: "i" },
-        province: provinceData._id,
-      });
+  //   //     return errorResponse({ message: "Invalid parameters" });
+  //   //   } catch (error) {
+  //   //     console.error("Error fetching data:", error);
+  //   //     return errorResponse({ message: "Đã có lỗi xảy ra", error: error.message });
+  //   //   }
+  //   // };
 
-      if (!districtData)
-        return errorResponse({ message: "District not found" });
+  //   // 2. Trường hợp chọn Province và District
+  //   if (province && district && !ward) {
+  //     const provinceData = await Province.findOne({
+  //       name: { $regex: province, $options: "i" },
+  //     });
+  //     const districtData = await District.findOne({
+  //       name: { $regex: district, $options: "i" },
+  //       province: provinceData._id,
+  //     });
 
-      const wards = await Ward.find({ district: districtData._id }).lean();
+  //     if (!districtData)
+  //       return errorResponse({ message: "District not found" });
 
-      const wardWithAnalysis = wards.map((w: any) => ({
-        name: w.name,
-        analysis: w.analysis[currentYear]?.[currentMonth] || null,
-      }));
+  //     const wards = await Ward.find({ district: districtData._id }).lean();
 
-      return successResponse({
-        data: {
-          province: provinceData.name,
-          district: districtData.name,
-          analysis: districtData.analysis[currentYear]?.[currentMonth] || null,
-          wards: wardWithAnalysis,
-        },
-        message: "Danh sách phường/xã và giá trị phân tích",
-      });
-    }
+  //     const wardWithAnalysis = wards.map((w: any) => ({
+  //       name: w.name,
+  //       analysis: w.analysis[currentYear]?.[currentMonth] || null,
+  //     }));
 
-    // 3. Trường hợp chọn Province, District và Ward
-    if (province && district && ward) {
-      const provinceData = await Province.findOne({
-        name: { $regex: province, $options: "i" },
-      });
-      const districtData = await District.findOne({
-        name: { $regex: district, $options: "i" },
-        province: provinceData._id,
-      });
+  //     return successResponse({
+  //       data: {
+  //         province: provinceData.name,
+  //         district: districtData.name,
+  //         analysis: districtData.analysis[currentYear]?.[currentMonth] || null,
+  //         wards: wardWithAnalysis,
+  //       },
+  //       message: "Danh sách phường/xã và giá trị phân tích",
+  //     });
+  //   }
 
-      if (!districtData)
-        return errorResponse({ message: "District not found" });
+  //   // 3. Trường hợp chọn Province, District và Ward
+  //   if (province && district && ward) {
+  //     const provinceData = await Province.findOne({
+  //       name: { $regex: province, $options: "i" },
+  //     });
+  //     const districtData = await District.findOne({
+  //       name: { $regex: district, $options: "i" },
+  //       province: provinceData._id,
+  //     });
 
-      const wards = await Ward.find({ district: districtData._id }).lean();
+  //     if (!districtData)
+  //       return errorResponse({ message: "District not found" });
 
-      const wardWithAnalysis = wards.map((w: any) => ({
-        name: w.name,
-        analysis: w.analysis[currentYear]?.[currentMonth] || null,
-      }));
+  //     const wards = await Ward.find({ district: districtData._id }).lean();
 
-      return successResponse({
-        data: {
-          province: provinceData.name,
-          district: districtData.name,
-          analysis: districtData.analysis[currentYear]?.[currentMonth] || null,
-          wards: wardWithAnalysis,
-        },
-        message: "Danh sách phường/xã và giá trị phân tích",
-      });
-    }
+  //     const wardWithAnalysis = wards.map((w: any) => ({
+  //       name: w.name,
+  //       analysis: w.analysis[currentYear]?.[currentMonth] || null,
+  //     }));
 
-    return errorResponse({ message: "Invalid parameters" });
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return errorResponse({ message: "Đã có lỗi xảy ra", error: error.message });
-  }
+  //     return successResponse({
+  //       data: {
+  //         province: provinceData.name,
+  //         district: districtData.name,
+  //         analysis: districtData.analysis[currentYear]?.[currentMonth] || null,
+  //         wards: wardWithAnalysis,
+  //       },
+  //       message: "Danh sách phường/xã và giá trị phân tích",
+  //     });
+  //   }
+
+  //   return errorResponse({ message: "Invalid parameters" });
+  // } catch (error) {
+  //   console.error("Error fetching data:", error);
+  //   return errorResponse({ message: "Đã có lỗi xảy ra", error: error.message });
+  // }
 };
