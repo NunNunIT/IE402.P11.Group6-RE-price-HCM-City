043ce792fe9195d42b7b023d8175613ae2930d85
fredle09 <le197903@gmail.com>
2024-12-21 05:31:58 +0700

    feat: add CustomizeMap component for enhanced map functionality in real estate details

---
+++ src/app/(page)/(user)/real-estate/[_id]/components/customize-map.tsx
@@ -0,0 +1,39 @@
+import { ENUM_MARKER_SYMBOL } from "@/utils";
+import dynamic from "next/dynamic";
+import useSWR from "swr";
+const GISMap = dynamic(() => import("@/components/gis-map"), { ssr: false });
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Failed to fetch data");
+  const payload = await res.json();
+  return payload.data;
+}
+
+export function CustomizeMap({ locate }: { locate: { lat: number, long: number } }) {
+  const { data } = useSWR(`/api/locations?sort=locate:${locate.lat},${locate.long}`, fetcher);
+
+  return (
+    <GISMap
+      zoom={20}
+      className="container"
+      isShowDistrict
+      center={locate}
+      points={[
+        ...(Array.isArray(data.rows)
+          ? data.rows.slice(0, 24).map((location: any) => ({
+            ...location.locate,
+            title: location.title,
+            type: location.category,
+          }))
+          : []
+        ),
+        {
+          ...data.locate,
+          title: data.title,
+          type: ENUM_MARKER_SYMBOL.REAL_ESTATE,
+        },
+      ]}
+    />
+  )
+}
