3eb9bfea43cb3224911db93f4813f5c246c761e6
NunNunIT <hongnhung02v04@gmail.com>
2024-12-20 19:59:57 +0700

    breadscrum

--- src/components/breadcrumb/index.tsx
+++ src/components/breadcrumb/index.tsx
@@ -12,40 +12,67 @@ import { usePathname } from "next/navigation";
 
 export default function BreadcrumbWithCustomSeparator() {
   const tranfer = [
-    { route: "/", label: ["Trang chủ"] },
-    { route: "/user", label: ["Trang chủ", "Tài khoản"] },
-    { route: "/real-estate", label: ["Trang chủ", "Bất động sản"] },
+    { pathRoute: "/", nav: [{ route: "/", label: "Trang chủ" }] },
     {
-      route: "/user/saved",
-      label: ["Trang chủ", "Tài khoản", "Bộ sưu tập lưu"],
+      pathRoute: "/user",
+      nav: [
+        { route: "/", label: "Trang chủ" },
+        { route: "/user", label: "Tài khoản" },
+      ],
     },
     {
-      route: "/user/my-re",
-      label: ["Trang chủ", "Tài khoản", "Bất động sản của tôi"],
+      pathRoute: "/real-estate",
+      nav: [
+        { route: "/", label: "Trang chủ" },
+        { route: "/real-estate", label: "Bất động sản" },
+      ],
     },
-    { route: "/analysis", label: ["Trang chủ", "Biến động"] },
-    { route: "/news", label: ["Trang chủ", "Tin tức"] },
+    {
+      pathRoute: "/user/saved",
+      nav: [
+        { route: "/", label: "Trang chủ" },
+        { route: "/user", label: "Tài khoản" },
+        { route: "/user/saved", label: "Bộ sưu tập lưu" },
+      ],
+    },
+    {
+      pathRoute: "/user/my-re",
+      nav: [
+        { route: "/", label: "Trang chủ" },
+        { route: "/user", label: "Tài khoản" },
+        { route: "/user/my-re", label: "Bất động sản của tôi" },
+      ],
+    },
+    {
+      pathRoute: "/analysis",
+      nav: [
+        { route: "/", label: "Trang chủ" },
+        { route: "/analysis", label: "Biến động" },
+      ],
+    },
+    {
+      pathRoute: "/news",
+      nav: [
+        { route: "/", label: "Trang chủ" },
+        { route: "/news", label: "Tin tức" },
+      ],
+    },
   ];
 
   const pathname = usePathname();
 
   // Extract the breadcrumb labels for the current path
   const breadcrumbLabels =
-    tranfer.find((item) => item.route === pathname)?.label || [];
+    tranfer.find((item) => item.pathRoute === pathname)?.nav || [];
 
   return (
     <div className="bg-white p-2 md:px-6 w-full">
       <Breadcrumb>
         <BreadcrumbList>
-          {breadcrumbLabels.map((label, index) => (
+          {breadcrumbLabels.map((nav, index) => (
             <BreadcrumbItem key={index}>
-              <BreadcrumbLink
-                href={
-                  tranfer.find((item) => item.label.includes(label))?.route ||
-                  "#"
-                }
-              >
-                {label}
+              <BreadcrumbLink href={nav.route || "#"}>
+                {nav.label}
               </BreadcrumbLink>
               {index < breadcrumbLabels.length - 1 && (
                 <BreadcrumbSeparator>
